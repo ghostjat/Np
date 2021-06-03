15c446b5b1b2a3fd429c4156bd683b0628cba5c8
ghostjat <ghost.jat@gmail.com>
2021-06-03 13:58:47 +0530

    namesapce update

--- examples/matrix.php
+++ examples/matrix.php
@@ -2,18 +2,16 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-use numphp\matrix;
+use Np\matrix;
 
 matrix::time();
 matrix::getMemory();
-$a = matrix::ar([[23,56,45],[89,98,55],[56,75,65]]);
-$b = \numphp\vector::ar([56,75,65]);
-
-echo $a->clip(0,1);
-echo PHP_EOL;
+$a = matrix::randn(1000, 1000);
+$b = matrix::randn(1000, 1000);
+$a->dot($b);
 matrix::getMemory();           // get memory use
 matrix::time();               // get time
 /**
  * Memory-Consumed 7.7mb
- * Time-Consumed:- 0.19370794296265
+ * Time-Consumed:- 0.18390893936157
  */
