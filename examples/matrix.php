ae783341835aee9b8d06ed432a8f3699182d8772
ghostjat <ghost.jat@gmail.com>
2021-06-05 13:22:30 +0530

    update static methods

--- examples/matrix.php
+++ examples/matrix.php
@@ -4,13 +4,11 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Np\matrix;
 
-matrix::time();
-matrix::getMemory();
 $a = matrix::randn(1000, 1000);
 $b = matrix::randn(1000, 1000);
 $a->dot($b);
-matrix::getMemory();           // get memory use
-matrix::time();               // get time
+$a->getMemory();           // get memory use
+$a->time();               // get time
 /**
  * Memory-Consumed 7.7mb
  * Time-Consumed:- 0.18390893936157
